8c121f7fed2fd2b295da2cda88342a95e697011d
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-06-16 16:39:11 +0100

    Update

--- .shell_preferences.sh
+++ .shell_preferences.sh
@@ -21,3 +21,8 @@ export EDITOR=/usr/bin/vim
 export VISUAL=/usr/bin/vim
 
 nvm use &> /dev/null || nvm use node &> /dev/null
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+#export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
