8321061da7131ea58a4e1cfb9d57496671069952
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-06-17 15:50:42 +0100

    Own that prompt

--- .shell_preferences.sh
+++ .shell_preferences.sh
@@ -16,10 +16,13 @@ export PATH=${PATH}:~/.pypy/bin/
 export PATH=${PATH}:~/bin/
 export EDITOR=/usr/bin/vim
 export VISUAL=/usr/bin/vim
+export WORKON_HOME=$HOME/.virtualenvs
+source /home/emil/.local/bin/virtualenvwrapper.sh &> /dev/null
 
 nvm use &> /dev/null || nvm use node &> /dev/null
 
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-#export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]'"\$(parse_git_branch)"'\$ '
