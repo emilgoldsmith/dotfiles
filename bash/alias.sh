fe31741ae5ef4e1335ad63e84cb9625eb6a1c912
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-02-25 16:36:21 +0100

    Automatic commit for changes of dotfiles

--- bash/alias.sh
+++ bash/alias.sh
@@ -4,7 +4,6 @@ alias firefox-no-verify="setsid firefox && sleep 1 && exit"
 alias chrome-no-verify="setsid google-chrome --disable-gpu-driver-bug-workarounds --enable-native-gpu-memory-buffers --enable-devtools-experiments && sleep 1 && exit"
 alias spotify="setsid spotify && sleep 1 && exit"
 alias reset-wifi="sudo service network-manager restart && exit"
-alias signal="setsid signal-desktop && sleep 10 && exit"
 alias add="git add -Ap && git status"
 alias code='GTK_IM_MODULE="xim" code'
 # Add an "alert" alias for long running commands.  Use like so:
