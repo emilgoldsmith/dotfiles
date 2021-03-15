d84f436d8de1eb60e1d4b8a857802cf6fdd71e7e
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-03-15 12:51:24 +0100

    Automatic commit for changes of dotfiles

--- install_personal_computer.sh
+++ install_personal_computer.sh
@@ -42,3 +42,15 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 # Allow us to run without sudo
 sudo groupadd --force docker
 sudo usermod -aG docker $USER
+
+########## Install VSCode
+
+# Taken from https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt install -y code
