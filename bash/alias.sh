9c55098f3c496a3ef02ae0c23824e2e9e5aade59
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-03-17 17:35:10 +0100

    Automatic commit for changes of dotfiles

--- bash/alias.sh
+++ bash/alias.sh
@@ -6,7 +6,6 @@ alias spotify="screen -d -m spotify && exit"
 alias reset-wifi="sudo service network-manager restart && exit"
 alias signal="screen -d -m signal-desktop && exit"
 alias add="git add -Ap && git status"
-alias code='GTK_IM_MODULE="xim" code'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
