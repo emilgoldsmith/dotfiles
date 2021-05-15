da1077e3c1e38250d2d758bfb9c3e2f78ee349fa
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-15 11:53:39 +0200

    Automatic commit for changes of dotfiles

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -20,3 +20,5 @@ commit_any_dotfile_changes
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(thefuck --alias)"
