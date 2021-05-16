23461eda0a5c37a656e2deb8b50d2a6feaea005b
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-16 19:35:10 +0200

    Automatic commit for changes of dotfiles

--- bash/alias.sh
+++ bash/alias.sh
@@ -13,6 +13,7 @@ alias dropbox="screen -d -m ~/.dropbox-dist/dropboxd"
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias amend='git commit --amend --no-edit'
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     eval "$(dircolors -b)"
