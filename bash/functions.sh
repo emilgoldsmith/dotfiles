19e5c9a3b9afb137420652baf251cb9a07d2b3c7
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-12-11 13:25:34 +0100

    Automatic commit for changes of dotfiles

--- bash/functions.sh
+++ bash/functions.sh
@@ -128,5 +128,5 @@ function commitAll() {
 
 function codeforces_contest_ratings() {
   curl "https://codeforces.com/api/contest.standings?contestId=$1&from=1&count=1&showUnofficial=false" | node -e "const x = JSON.parse(require('fs').readFileSync('/dev/stdin', 'utf-8').toString());\
-    console.log({contestName: x.result.contest.name, problems: x.result.problems.map(y => {name: y.name, rating: y.rating})});"
+    console.log({contestName: x.result.contest.name, problems: x.result.problems.map(y => ({name: y.name, rating: y.rating}))});"
 }
