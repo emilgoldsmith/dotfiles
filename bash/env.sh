8bf281fa285b9f66308481ca8ed52961402c6a54
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2023-06-07 12:12:18 +0200

    Automatic commit for changes of dotfiles

--- bash/env.sh
+++ bash/env.sh
@@ -4,3 +4,4 @@ export VISUAL=/usr/bin/vim
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export PATH=$PATH:~/bin
 export PATH=$PATH:~/.local/bin
+export PATH=$PATH:/usr/local/go/bin
