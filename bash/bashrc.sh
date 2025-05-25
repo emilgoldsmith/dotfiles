17aec8ae15e05135559fe9b14c753b91847e3bcc
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2025-05-24 17:06:34 -0700

    Automatic commit for changes of dotfiles

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -8,6 +8,9 @@ case $- in
 *) return ;;
 esac
 
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
